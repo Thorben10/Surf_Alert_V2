8d0a466d1d916f8b29efe097aee4870a53100947
Thorben10 <thorben_Steinberg@web.de>
2026-06-30 11:56:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,15 @@
+# config.py
 
+# Telegram
+BOT_TOKEN = None
+CHAT_ID = None
+
+# Forecast
+FORECAST_PROVIDER = "open-meteo"
+
+# Allgemein
+MIN_SESSION_LENGTH = 3
+DEFAULT_TIMEZONE = "Europe/Berlin"
+
+# Debug
+DEBUG = True
